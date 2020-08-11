d80cfb27e121e5fe4ec26924b371cb0c332a9cbb
朝洋 s <15657868169m@sina.cn>
2020-08-11 15:24:46 +0800

    Formate code.

---
+++ Tk.Soc.LAS/LasHelper/LocalTestHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Tk.Soc.LAS.UnitTest;
+
+namespace Tk.Soc.LAS.LasHelper
+{
+    public static class LocalTestHelper
+    {
+        public static void OnStart()
+        {
+
+            //DbHelper.InsertDataIntoMysql(1000);
+
+            string path = @"D:\WorkMaterial\日常\2020-8-11-性能测试\Conf\test.txt";
+
+            Console.WriteLine("-----数据库现有数据 {0} 条------\n", DbHelper.GetCompanyConut());
+
+            try
+            {
+                StreamReader sr = new StreamReader(path, Encoding.Default);
+                String line = sr.ReadLine();
+                int limit = 10;
+
+                if (!string.IsNullOrEmpty(line))
+                {
+                    int count = 0;
+                    string strId = line.Replace("--- ", "");
+                    int id = int.Parse(strId.Trim());
+
+                    while (true)
+                    {
+                        DbHelper.DelectCompanyDataAsync(id, limit);
+                        Console.WriteLine("成功第 {0} 次删除数据。", ++count);
+
+                        int companyCount = DbHelper.GetCompanyConut();
+                        Console.WriteLine("数据库剩余 {0} 条数据", companyCount);
+
+                        id = id >= limit ? id - limit : 0;
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("读取数据失败");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("读取数据失败，异常如下：\n{0}", e.ToString());
+            }
+
+            //Console.WriteLine(DbHelper.GetCompanyConut());
+            Console.ReadKey();
+        }
+    }
+}
